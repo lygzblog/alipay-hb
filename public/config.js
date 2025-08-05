67ed523d8ebd87c6f9fced995dd8b28e44e46009
lygz <2805049491@qq.com>
2025-08-05 18:41:05 +0800

    优化页面跳转

---
+++ public/config.js
@@ -0,0 +1,10 @@
+window.ENV_CONFIG = {
+  // 支付宝用户ID
+  VITE_ALIPAY_USER_ID: "2088922557117308",
+  // 赏金二维码token
+  VITE_QR_TOKEN: "AhGZ19610515y4vfkzko7jotd85w2O"
+};
+
+// 作者：羚羊公子
+// 配置文档：https://lygzblog.feishu.cn/wiki/HlcQw1Za1i2Gnek8ToycVbM2nbe?from=from_copylink
+
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: 羚羊公子
+ * @FilePath: \alipay-hb\vite.config.js
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    open: true,//启动项目自动弹出浏览器
+    port: "8080",
+    proxy: {
+      '/api': {
+        // target: 'https://frp.chainroad.cn',
+        // target: 'http://127.0.0.1:8888',
+        changeOrigin: true,  // 允许跨域
+        rewrite: path => path.replace(/^\/api/, '')
+      },
+    },
+    allowedHosts: [
+      'chainroad.cn', // 允许访问的域名地址，即frp网穿透的地址
+      '.chainroad.cn'   // .是二级域名的通配符   
+    ],
+  }
+})
